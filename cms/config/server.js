52d3db989f6c30ef867059e14b00e41fd7bb1863
Vincent Guizot <vincentguizot128@gmail.com>
2021-02-09 18:14:10 +0700

    init

---
+++ cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c2946fa6fdcc3a39981076d4c68ad09b'),
+    },
+  },
+});
---
+++ cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '006f8ee4-dd58-44e1-9b16-53ca5989e41f'
+};
